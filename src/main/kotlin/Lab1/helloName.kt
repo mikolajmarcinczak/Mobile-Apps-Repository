e3b6cfc95723d060d53dff2b517b2a4fbc3efbb5
mikolajmarcinczak <mikolajtechhnika@gmail.com>
2023-10-21 14:59:25 +0200

    commit

---
+++ src/main/kotlin/Lab1/helloName.kt
@@ -0,0 +1,5 @@
+package com.mobile.lab1
+
+fun helloName() {
+    println("Hello, ${readln()}. Have a nice day!")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package com.mobile
+
+import com.mobile.lab1.*
+
+fun main(args: Array<String>) {
+    //zad 2.
+    helloName()
+
+    //zad 3.
+
+}
